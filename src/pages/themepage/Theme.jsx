e468d460fc36cbd593f8a98618499f4d24529b92
artisonii <artisoni2615@gmail.com>
2024-04-08 22:18:18 +0530

    initial commit1

--- src/pages/themepage/Theme.jsx
+++ src/pages/themepage/Theme.jsx
@@ -2,7 +2,7 @@ import React, { useContext, useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { DataContext } from '../../context/DataContext'
 const Theme = () => {
-    const { data } = useContext(DataContext)
+    const { data, setData } = useContext(DataContext)
     const navigate = useNavigate()
     const handleClick = (e) => {
         const val = data.theme.filter((ele) => {
